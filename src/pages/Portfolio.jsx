090cc3b0221750f5c0c003eea016ea361a364e0f
TavonnS <142205891+TavonnS@users.noreply.github.com>
2024-02-16 11:16:13 -0800

    repathing for deploy

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -7,25 +7,25 @@ const Portfolio = () => {
       title: 'Text Writer',
       githubLink: 'https://github.com/TavonnS/pwa-writer-app',
       deployedLink: 'https://writer-app.onrender.com/',
-      image: './public/writer.png',
+      image: './dist/writer.png',
     },
     {
       title: 'Blog',
       githubLink: 'https://github.com/TavonnS/tech-blog',
       deployedLink: 'https://dry-river-45552-65f9dd09dfff.herokuapp.com/login',
-      image: './public/techblog.png',
+      image: './dist/techblog.png',
     },
     {
       title: 'Travel Log',
       githubLink: 'https://github.com/Jdrei01/travel-log-app',
       deployedLink: 'https://travel-log-app-2f2b22f463d1.herokuapp.com/',
-      image: './public/travelapp.png',
+      image: './dist/travelapp.png',
     },
     {
       title: 'Weather Report',
       githubLink: 'https://github.com/TavonnS/weather-report-app',
       deployedLink: 'https://tavonns.github.io/weather-report-app/',
-      image: './public/weather.png',
+      image: './dist/weather.png',
     },
   ];
 
